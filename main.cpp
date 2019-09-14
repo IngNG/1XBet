54f83dfca8cfd1c0bdc76a74af8eb8b4c11e1d1b
Mikhail Abramov <beavisabra@yandex.ru>
2019-09-14 20:57:55 +0400

    Структура проекта

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include "TXLib.h"
+#include "Lib\\TXLib.h"
 
 int main()
-    {
+{
     txCreateWindow (1000, 800);
+
     txSetColor(TX_RED);
-    txRectangle(100, 100, 350, 300);
-    txRectangle(600, 100, 850, 300);
+    txRectangle(100, 200, 350, 400);
+    txRectangle(600, 200, 850, 400);
+    txRectangle(100, 500, 350, 700);
+    txRectangle(600, 500, 850, 700);
 
     return 0;
-    }
+}
 
