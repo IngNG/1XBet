b7d01339d10fd3c0ec28dbcf24c1425acba9994a
kot007 <55321443+kot007@users.noreply.github.com>
2019-09-25 19:55:30 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include "TXLib.h"
+   struct  Picture
+   {
+    int x  ;
+    int y ;
+    int shirina;
+    int vasota;
+    HDC pic;
+   };
+
+   void drawPic (Picture vk)
+   {
+    txBitBlt(txDC(), vk.x, vk.y, vk.shirina, vk.vasota, vk.pic);
+   }
 
 bool knopka(int x, int y)
 {
@@ -19,89 +32,113 @@ int main()
 {
     txCreateWindow (1200, 800);
 
+    HDC  brick= txLoadImage ("Pics\\Wall\\brick.bmp");
+    HDC wood = txLoadImage ("Pics\\Wall\\wood.bmp");
     HDC  background  = txLoadImage ("Pics\\фон 2.bmp");
 
     bool exitProgram = false;
+
+    int pageNumber = 0;
+
+
     while(!exitProgram)
     {
         txBegin();
-        txBitBlt (txDC(), 0, 0, 1200, 800, background, 0, 0);
-        txSetFillColor(TX_WHITE);
-        txSetColor(TX_WHITE);
-        /*Win32::RoundRect (txDC(), 450, 230, 750,730,50,50); */
 
-        txSelectFont("Arial", 40);
-        txDrawText(300,100,900,150,"Создай свою квартиру");
-
-        txDrawText(500,270,680,320,"Начать");
-        if(knopka(530,270))
+        if (pageNumber == 0)
         {
-            txMessageBox("Загрузка");
-        }
+			txBitBlt (txDC(), 0, 0, 1200, 800, background, 0, 0);
+			txSetFillColor(TX_WHITE);
+			txSetColor(TX_WHITE);
+			/*Win32::RoundRect (txDC(), 450, 230, 750,730,50,50); */
 
-        txDrawText(500,320,680,370,"Продолжить");
-        if (knopka(530,320))
-        {
-            txMessageBox("Загрузка");
-        }
+			txSelectFont("Arial", 40);
+			txDrawText(300,100,900,150,"Создай свою квартиру");
 
-        txDrawText(500,370,680,420,"Настройки");
-        if (knopka(530,370))
-        {
-            txMessageBox("настройки недоступны");
-        }
+			txDrawText(500,270,680,320,"Начать");
+			if(knopka(530,270))
+			{
+				 if (pageNumber = 1);
+			}
 
-        txDrawText(500,420,680,470,"Информация");
-        if (knopka(530,420))
-        {
-            txMessageBox("Cейчас найдём");
-        }
+			txDrawText(500,320,680,370,"Продолжить");
+			if (knopka(530,320))
+			{
+				txMessageBox("Загрузка");
+			}
 
-        txDrawText(500,470,680,520,"Выйти");
-        if (knopka(530,470))
-        {
-            exitProgram = true;
-        }
+			txDrawText(500,370,680,420,"Настройки");
+			if (knopka(530,370))
+			{
+				txMessageBox("настройки недоступны");
+			}
+
+			txDrawText(500,420,680,470,"Информация");
+			if (knopka(530,420))
+			{
+				txMessageBox("Cейчас найдём");
+			}
+
+			txDrawText(500,470,680,520,"Выйти");
+			if (knopka(530,470))
+			{
+				exitProgram = true;
+			}
+		}
 
         /* txSetFillColor (TX_YELLOW);
 
         txRectangle (425, 270, 775, 382); */
 
+        if (pageNumber == 1 )
+        {
+
+				txSetFillColor (TX_GRAY);
+				txRectangle ( 10 , 100 , 1200 - 150 , 800 - 2 );
+
+
+				txDrawText(55,20,180,70,"диваны");
+			if(knopka(55,20))
+			{
+			}
+
+			  /*
+				HDC  pic = txLoadImage ("Pics\\Furniture\\Диван 1.bmp");
+				Win32::TransparentBlt (txDC(), 1090, 110, 75, 190, pic, 0, 0, 185, 451, TX_WHITE);
+				txDeleteDC(pic);
+
+				HDC  pic2 = txLoadImage ("Pics\\Furniture\\Диван 2.bmp");
+				Win32::TransparentBlt (txDC(), 1090, 320, 75, 190, pic2, 0, 0, 185, 451, TX_WHITE);
+				txDeleteDC(pic2);
+
+				HDC  pic3 = txLoadImage ("Pics\\Furniture\\Диван 3.bmp");
+				Win32::TransparentBlt (txDC(), 1060, 540, 120, 60, pic3, 0, 0, 451, 185, TX_WHITE);
+				txDeleteDC(pic3);
+
+				HDC  pic4 = txLoadImage ("Pics\\Furniture\\Диван 4.bmp");
+				Win32::TransparentBlt (txDC(), 1060, 630, 120, 60, pic4, 0, 0, 451, 185, TX_WHITE);
+				txDeleteDC(pic4);
+								   */
+
+				Win32::TransparentBlt (txDC(), 1060, 630, 100, 100, brick, 0, 0, 100, 100, TX_WHITE);
+
+				Win32::TransparentBlt (txDC(), 1060, 520, 100, 100, wood, 0, 0, 100, 100, TX_WHITE);
+        }
+
+
 
         txSleep(10);
         txEnd();
+
+
+
     }
 
     txDisableAutoPause();
     txDeleteDC(background);
 
 
-    txSetFillColor (TX_GRAY);
-    txRectangle ( 10 , 100 , 1200 - 150 , 800 - 2 );
-  /*
-    HDC  pic = txLoadImage ("Pics\\Furniture\\Диван 1.bmp");
-    Win32::TransparentBlt (txDC(), 1090, 110, 75, 190, pic, 0, 0, 185, 451, TX_WHITE);
-    txDeleteDC(pic);
-
-    HDC  pic2 = txLoadImage ("Pics\\Furniture\\Диван 2.bmp");
-    Win32::TransparentBlt (txDC(), 1090, 320, 75, 190, pic2, 0, 0, 185, 451, TX_WHITE);
-    txDeleteDC(pic2);
-
-    HDC  pic3 = txLoadImage ("Pics\\Furniture\\Диван 3.bmp");
-    Win32::TransparentBlt (txDC(), 1060, 540, 120, 60, pic3, 0, 0, 451, 185, TX_WHITE);
-    txDeleteDC(pic3);
-
-    HDC  pic4 = txLoadImage ("Pics\\Furniture\\Диван 4.bmp");
-    Win32::TransparentBlt (txDC(), 1060, 630, 120, 60, pic4, 0, 0, 451, 185, TX_WHITE);
-    txDeleteDC(pic4);
-                       */
-
-    HDC  brick= txLoadImage ("Pics\\Wall\\brick.bmp");
-    Win32::TransparentBlt (txDC(), 1060, 630, 100, 100, brick, 0, 0, 100, 100, TX_WHITE);
     txDeleteDC(brick);
-
-    HDC wood = txLoadImage ("Pics\\Wall\\wood.bmp");
-    Win32::TransparentBlt (txDC(), 1060, 520, 100, 100, wood, 0, 0, 100, 100, TX_WHITE);
     txDeleteDC(wood);
     txSleep(2000);
 
