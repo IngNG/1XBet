c830a27504f8af1d1028656470e533d5a5bbca4e
Chirakov <50415177+Chirakov@users.noreply.github.com>
2019-10-09 19:55:54 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,38 +11,22 @@
 int main()
 {
     txCreateWindow (1200, 800);
-
-    HDC  brick= txLoadImage ("Pics\\Wall\\brick.bmp");
-    HDC wood = txLoadImage ("Pics\\Wall\\wood.bmp");
     HDC  background  = txLoadImage ("Pics\\фон 2.bmp");
 
-    HDC  pic1 = txLoadImage ("Pics\\Furniture\\Диван1.bmp");
-    HDC  pic2 = txLoadImage ("Pics\\Furniture\\Диван2.bmp");
-    HDC  pic3 = txLoadImage ("Pics\\Furniture\\Диван3.bmp");
-    HDC  pic4 = txLoadImage ("Pics\\Furniture\\Диван4.bmp");
-
-    bool exitProgram = false;
-    Picture sofa[5];
-    sofa[0] = {390, 110, 75, 190, pic1, 185, 451};
-    bool mapvisible = false;
-
-     Picture sofa2[5];
-    sofa[1] = {390, 320, 75, 190, pic2, 185, 451};
-    bool mapvisible2 = false;
-
-
-
-
-    Picture pic[4];
-    pic[0] = {1090, 110, 75, 190, pic1, 185, 451};
-    pic[1] = {1090, 320, 75, 190, pic2, 185, 451};
-    bool visible = false;
-
-    Picture cover[5];
-    cover[0] = {1090, 110, 75, 190,  txLoadImage ("Pics\\ковёр1.bmp"), 300, 224};
-    cover[1] = {1090, 320, 75, 190,  txLoadImage ("Pics\\ковёр3.bmp"), 350, 350};
-    bool visible2 = false;
-
+    char* category;
+    Picture pic[15];
+    pic[0] = {1090, 110, 75, 190, txLoadImage ("Pics\\Furniture\\Диван1.bmp"), 185, 450, "Диван"};
+    pic[1] = {1090, 320, 75, 190, txLoadImage ("Pics\\Furniture\\Диван2.bmp"), 185, 451, "Диван"};
+    pic[2] = {1090, 110, 75, 190, txLoadImage ("Pics\\ковёр1.bmp"), 300, 224, "Ковер"};
+    pic[3] = {1090, 320, 75, 190, txLoadImage ("Pics\\ковёр3.bmp"), 350, 350, "Ковер"};
+    pic[4] = {1070, 580, 120,50,  txLoadImage ("Pics\\Furniture\\Диван3.bmp"), 451, 185, "Диван"};
+    pic[5] = {1070, 640, 120,50,  txLoadImage ("Pics\\Furniture\\Диван4.bmp"), 451, 185, "Диван"};
+    pic[6] = {1090, 110, 75, 190, txLoadImage ("Pics\\Furniture\\кресло1.bmp"), 350, 250, "кресло"};
+    pic[7] = {1090, 320, 75, 190, txLoadImage ("Pics\\Furniture\\кресло2.bmp"), 250, 150, "кресло"};
+    pic[8] = {1090, 580, 75, 190, txLoadImage ("Pics\\Furniture\\кресло3.bmp"), 150, 150, "кресло"};
+    pic[9] = {1090, 110, 75, 75, txLoadImage ("Pics\\Wall\\brick.bmp"),100, 100, "стены"};
+    pic[10] = {1090,320, 75, 75, txLoadImage ("Pics\\Wall\\mel.bmp")  ,100, 100, "стены"};
+    pic[11] = {1090,580, 75, 75, txLoadImage ("Pics\\Wall\\wood.bmp") ,100, 100, "стены"};
     int pageNumber = 0;
 
     Knopka buttons[6];
@@ -52,6 +36,7 @@ int main()
     buttons[3] = {500,420,680,470,"Информация","Cейчас найдём"};
     buttons[4] = {500,470,680,520,"Выйти",""};
 
+    bool exitProgram = false;
 
     while(!exitProgram)
     {
@@ -87,6 +72,7 @@ int main()
 
         if (pageNumber == 1 )
         {
+            txBitBlt (txDC(), 0, 0, 1200, 800, background, 0, 0);
             txSetFillColor (TX_GRAY);
             txRectangle ( 10 , 100 , 1200 - 150 , 800 - 2 );
 
@@ -95,8 +81,7 @@ int main()
             txDrawText(55,20,180,70,"диваны");
             if(knopka(55,20))
             {
-            visible = true;
-            visible2 = false;
+            category = "Диван";
             txSleep(200);
             }
 
@@ -108,58 +93,41 @@ int main()
             txDrawText(455,20,580,70,"стены");
             if(knopka(440,40))
             {
+            category = "стены";
+            txSleep(200);
             }
 
-            txDrawText(655,20,780,70,"техника");
+            txDrawText(655,20,780,70,"кресло");
             if(knopka(640,40))
             {
-
+            category = "кресло";
+            txSleep(200);
             }
 
             txDrawText(855,20,980,70,"ковры");
             if(knopka(840,40))
             {
-            visible2 = true;
-            visible = false;
             txSleep(200);
+            category = "Ковер";
             }
             }
 
+
 			//Кнопки справа
-            if(knopka(1090,200))
-            {
-            mapvisible = !mapvisible;
-            txSleep(200);
-            }
-              if(knopka(1090,400))
-            {
-            mapvisible2 = !mapvisible2;
-            txSleep(200);
-            }
+
+for (int i = 0; i<15; i++)
+{
+ if (pic[i].category == category)
+ {
+  drawPic(pic[i]);
+ }
+}
+
 
 
             if (GetAsyncKeyState(VK_ESCAPE))
             {
                 exitProgram = true;
-            }
-             if (visible)
-             {
-            drawPic(pic[0]);
-            drawPic(pic[1]);
-            }
-             if (visible2)
-             {
-            drawPic(cover[0]);
-            drawPic(cover[1]);
-            }
-
-             if (mapvisible)
-             {
-            drawPic(sofa[0]);
-            }
-             if (mapvisible2)
-             {
-            drawPic(sofa[1]);
             }
         }
 
@@ -176,13 +144,20 @@ int main()
     txDeleteDC(background);
 
 
-    txDeleteDC(brick);
-    txDeleteDC(wood);
+    txDeleteDC(pic[0].img);
+    txDeleteDC(pic[1].img);
+    txDeleteDC(pic[2].img);
+    txDeleteDC(pic[3].img);
+    txDeleteDC(pic[4].img);
+    txDeleteDC(pic[5].img);
+    txDeleteDC(pic[6].img);
+    txDeleteDC(pic[7].img);
+    txDeleteDC(pic[8].img);
+    txDeleteDC(pic[9].img);
+    txDeleteDC(pic[10].img);
+    txDeleteDC(pic[11].img);
+                              ;
 
-    txDeleteDC(pic1);
-    txDeleteDC(pic2);
-    txDeleteDC(pic3);
-    txDeleteDC(pic4);
 
 
 
--- picture.cpp
+++ picture.cpp
@@ -8,9 +8,14 @@ struct  Picture
     HDC img;
     int src_shirina;
     int src_vasota;
+    char* category ;
+
 };
 
 void drawPic (Picture vk)
 {
     Win32::TransparentBlt (txDC(), vk.x, vk.y, vk.shirina, vk.vasota, vk.img, 0, 0, vk.src_shirina, vk.src_vasota, TX_WHITE);
 }
+
+
+
